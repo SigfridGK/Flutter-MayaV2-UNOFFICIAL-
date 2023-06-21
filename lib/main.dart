a6e5fd7bab79b866e582a98db0e8b3125faa5173
Sigfrid <Sigfrid.Acabal@goodkredit.com>
2023-06-21 11:01:29 +0800

    First Init

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+
+  @override
+  State<MyApp> createState() => _MyApp();
+}
+
+class _MyApp extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return
+    Scaffold();
+  }
+}
